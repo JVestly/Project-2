f15ca01873aa40dfe1ac840c73c83021f1806553
JVestly <122886073+JVestly@users.noreply.github.com>
2025-10-22 15:51:44 +0200

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,21 @@
+def mse():
+    """TODO: Complete docstring. Reuse code from P1"""
+    return None
+
+def cross_entropy():
+    """TODO: Complete docstring and code"""
+    return None
+
+def softmax():
+    """TODO: Complete docstring and code"""
+    return None
+
+def sigmoid():
+    """TODO: Complete docstring and code"""
+    return None
+
+def reLU():
+    return None
+
+def leaky_reLU():
+    return None
