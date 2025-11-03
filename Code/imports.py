9afed1166e33288d1bef5dbd5a85e5715c0c6066
JVestly <Jorgetve@ifi.uio.no>
2025-11-03 14:00:52 +0100

    Added imports

--- Code/imports.py
+++ Code/imports.py
@@ -7,3 +7,12 @@ from autograd import grad
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from numpy.linalg import pinv
+import os
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from autograd import grad
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.linear_model import LinearRegression
+from sklearn.neural_network import MLPRegressor
